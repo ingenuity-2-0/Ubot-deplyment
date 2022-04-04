15158f2499a04d174214e658e7d4f3fd098d7447
Sabbir Hosen <18201076@uap-bd.edu>
2022-04-04 21:02:04 +0600

    fake api created 1

--- app.py
+++ app.py
@@ -2,9 +2,14 @@ from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
+
 # @app.get('/')
 # def index_get():
 #     return render_template('base.html')
+
+@app.route('/')
+def home():
+    return 'Testing of Ubot Fake API'
 
 
 @app.route('/<string:text>')
